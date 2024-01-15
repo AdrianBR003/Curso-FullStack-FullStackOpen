bf1a0da02985fe258c141cd6882e82d431005413
AdrianBR003 <149155965+AdrianBR003@users.noreply.github.com>
2024-01-15 13:31:23 +0100

    Ap d - 2/2 Controladores de eventos a componentes hijos

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,58 +1,26 @@
 import { useState } from 'react'
 
-/*
-const Display = ( {counter} ) =>{
-  return(
-    <div>{counter}</div>
-  )
-}
-*/
-const Button = ( {handleClick, text} ) =>( <button onClick={handleClick}>{text}</button> )
-
-
-const History = (props) => {
-  if(props.allClicks.length === 0){
-    return(<div>
-      the app is used by pressing the buttons
-    </div>
-    )
-  }
-  return(<div>
-      button press History: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
 const App = () => {
+  const [value, setValue] = useState(10)
 
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([]) //Inicializa con la matriz vacia
-  const [cont, setCont] = useState(0) //Nos informa sobre el numero de veces que pulsamos los botones
-
-  const handleLeftClick = () =>{
-    setAll(allClicks.concat('L'))
-    setLeft(left+1)
-    setCont(cont+1)
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
-  const handleRightClick = () =>{
-    setAll(allClicks.concat('R'))
-    setRight(right+1)
-    setCont(cont+1)
-  }
-
-  return (<>
-    {left}
-    <Button handleClick={handleLeftClick} text='left'/>
-    <Button handleClick={handleRightClick} text='right'/>
-    {right}
-    <br /> 
-    <br />
-    <History allClicks={allClicks} /> 
-    <p>Contado de Pulsaciones: {cont}</p>
-    </>
+  return (
+    <div>
+      {value}
+      <button onClick={() => setToValue(1000)}>
+        thousand
+      </button>
+      <button onClick={() => setToValue(0)}>
+        reset
+      </button>
+      <button onClick={() => setToValue(value + 1)}>
+        increment
+      </button>
+    </div>
   )
 }
-
 export default App
